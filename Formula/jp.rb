72eea9608569eb967aefdcf1a9f3f443531efb1a
Paul Hammond <paul@paulhammond.org>
2020-01-27 20:53:59 -0800

    Homebrew now uses sha256

--- Formula/jp.rb
+++ Formula/jp.rb
@@ -4,7 +4,7 @@ class Jp < Formula
   homepage 'http://www.paulhammond.org/jp/'
   url 'https://github.com/paulhammond/jp/releases/download/v0.2/mac.tgz'
   version '0.2'
-  sha1 '65e5cb43af3a7f4e1567914c53b764aca204714c'
+  sha256 '6346d78b64804974434c9518f3b2c0862d1545af773e9b740a9e781f9d046a88'
 
   def install
     bin.install 'jp'
