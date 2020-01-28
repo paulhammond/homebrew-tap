7ef03a4c0d37523a034ba88dc9e770cc214a5111
Paul Hammond <paul@paulhammond.org>
2020-01-27 22:13:20 -0800

    jp v0.3

--- Formula/jp.rb
+++ Formula/jp.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Jp < Formula
   homepage 'http://www.paulhammond.org/jp/'
-  url 'https://github.com/paulhammond/jp/releases/download/v0.2/mac.tgz'
-  version '0.2'
-  sha256 '6346d78b64804974434c9518f3b2c0862d1545af773e9b740a9e781f9d046a88'
+  url 'https://github.com/paulhammond/jp/releases/download/v0.3/jp-mac-v0.3.tgz'
+  version '0.3'
+  sha256 '617ece7a704423d3230f27d763f2bfe47d9edd07b0dadf44079b1c0151a527c0'
 
   def install
     bin.install 'jp'
