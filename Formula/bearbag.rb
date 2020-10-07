4c58d3c95fa5ade5d39f2697339ae938288660c2
Paul Hammond <paul@paulhammond.org>
2020-10-07 13:30:49 -0700

    bearbag v0.1

---
+++ Formula/bearbag.rb
@@ -0,0 +1,20 @@
+class Bearbag < Formula
+  desc "Bear export utility"
+  homepage "https://github.com/paulhammond/bearbag"
+  url "https://github.com/paulhammond/bearbag.git",
+    revision: "035475c65a75c64cfc867efca983c95f1fd4a9e1",
+    tag:      "v0.1"
+  license "MIT"
+  head "https://github.com/paulhammond/bearbag.git"
+
+  depends_on xcode: ["11.0", :build]
+
+  def install
+    system "swift", "build", "--disable-sandbox", "-c", "release", "--product", "bearbag"
+    bin.install ".build/release/bearbag"
+  end
+
+  test do
+    system "#{bin}/bearbag", "--help"
+  end
+end
