62ee3eb5605706da4f3d110f05cc1f52d0ae618e
Paul Hammond <paul@paulhammond.org>
2020-10-27 17:06:45 -0700

    jp: switch to compiling from source

--- Formula/jp.rb
+++ Formula/jp.rb
@@ -1,13 +1,16 @@
-require 'formula'
-
 class Jp < Formula
-  homepage 'http://www.paulhammond.org/jp/'
-  url 'https://github.com/paulhammond/jp/releases/download/v0.3/jp-mac-v0.3.tgz'
-  version '0.3'
-  sha256 '617ece7a704423d3230f27d763f2bfe47d9edd07b0dadf44079b1c0151a527c0'
+  homepage 'https://paulhammond.org/jp'
+  url "https://github.com/paulhammond/jp.git",
+    revision: "8ad2851b05f0b89b60512970700140967ad14ec9",
+    tag:      "v0.3"
+  revision 1
+  head "https://github.com/paulhammond/jp.git"
+  license "MIT"
+
+  depends_on "go" => :build
 
   def install
-    bin.install 'jp'
+    system "go", "build", "-o", bin/"jp", "./jp"
   end
 
   test do
